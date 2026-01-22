cf0279901bd4970c1eeed0b1213b90d4962ce314
Pablo Calle <callejupa@gmail.com>
2026-01-22 17:36:56 -0500

    server in nodejs

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('initial configuration')
+import http from 'node:http'
+
+const PORT = 8080
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server')
+
+    console.log(req.url)
+})
+
+server.listen(PORT, () => {
+    console.log(`server running on port: ${PORT}`)
+})
