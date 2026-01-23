4defac304924f345e571d42eeae44f6cf589eb0f
Pablo Calle <callejupa@gmail.com>
2026-01-22 19:07:01 -0500

    rout not found

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB()
 
     if(req.url === '/api' && req.method === 'GET') {
-
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 200
-    res.end(JSON.stringify(destinations))
-  }
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 200
+        res.end(JSON.stringify(destinations))
+    } else {
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 404
+        res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"}))
+    }
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
